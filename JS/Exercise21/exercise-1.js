6a3d78f2b5bec38a7820c861f04627a2366a656f
Alptekin <alptekin.ocakdan@gmail.com>
2023-05-23 13:01:25 +0300

    Update exercise-1.js

--- JS/Exercise21/exercise-1.js
+++ JS/Exercise21/exercise-1.js
@@ -1,8 +1,8 @@
 let num1 = 10;
 let num2 = 20;
-console.log('Before swap: ', num1, num2); // Before swap: 10 20
+console.log('Before swap: ', num1, num2);
 
-// Variable swapping using destructuring assignment
+
 [num1, num2] = [num2, num1];
 
-console.log('After swap: ', num1, num2); // After swap: 20 10
+console.log('After swap: ', num1, num2);
