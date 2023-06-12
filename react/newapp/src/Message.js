45b980f44187771d31c7a28123448f32ed536c2b
Alptekin <alptekin.ocakdan@gmail.com>
2023-06-12 14:36:07 +0300

    React - Exercise 1

---
+++ react/newapp/src/Message.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Message extends Component {
+    render() {
+        return (
+            <div>
+                <p>It's time to learn React!!!</p>
+            </div>
+        );
+    }
+}
+
+export default Message;
