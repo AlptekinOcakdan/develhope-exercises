4610fb3bd3b2495ca1b20056181f54fdaf2f9c82
Alptekin <alptekin.ocakdan@gmail.com>
2023-06-14 14:53:53 +0300

    name changes

--- react/exercise3/src/App.js
+++ react/exercise3/src/App.js
@@ -1,7 +1,7 @@
 import Welcome from "./Welcome";
 
 function App() {
-    return <Welcome name="John" age={30} />;
+    return <Welcome name="Alptekin" age={23} />;
 }
 
 export default App;
--- react/exercise5/src/App.js
+++ react/exercise5/src/App.js
@@ -1,7 +1,7 @@
 import Welcome from "./Welcome";
 
 function App() {
-    return <Welcome name={<strong>John</strong>} age={30} />;
+    return <Welcome name={<strong>Alptekin</strong>} age={23} />;
 }
 
 export default App;
