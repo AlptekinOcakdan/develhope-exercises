44042351419853d1670fdfc3000e9f2b9bc2a280
Alptekin <alptekin.ocakdan@gmail.com>
2023-06-15 19:59:31 +0300

    Forms - `UncontrolledLogin` component

---
+++ react/exercise21/src/App.js
@@ -0,0 +1,11 @@
+import UncontrolledLogin from "./UncontrolledLogin";
+
+function App() {
+    return (
+        <div>
+            <UncontrolledLogin/>
+        </div>
+    );
+}
+
+export default App;
---
+++ react/exercise21/src/UncontrolledLogin.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+class UncontrolledLogin extends Component {
+    constructor(props) {
+        super(props);
+        this.usernameRef = React.createRef();
+        this.passwordRef = React.createRef();
+    }
+
+    handleLogin = () => {
+        const username = this.usernameRef.current.value;
+        const password = this.passwordRef.current.value;
+        const { onLogin } = this.props;
+        onLogin({ username, password });
+    };
+
+    handleReset = () => {
+        this.usernameRef.current.value = '';
+        this.passwordRef.current.value = '';
+    };
+
+    render() {
+        return (
+            <div>
+                <input
+                    type="text"
+                    ref={this.usernameRef}
+                    placeholder="Username"
+                />
+                <input
+                    type="password"
+                    ref={this.passwordRef}
+                    placeholder="Password"
+                />
+                <button onClick={this.handleLogin}>
+                    Login
+                </button>
+                <button onClick={this.handleReset}>
+                    Reset
+                </button>
+            </div>
+        );
+    }
+}
+
+export default UncontrolledLogin;
