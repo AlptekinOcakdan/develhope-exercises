e20b7cab648dd1eec10cbf8566aa9bc7e7364451
Alptekin <alptekin.ocakdan@gmail.com>
2023-06-15 14:39:15 +0300

    Forms - Add "login" `button`

---
+++ react/exercise19/src/Login.js
@@ -0,0 +1,56 @@
+import { Component } from 'react';
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: ''
+        };
+    }
+
+    handleUsernameChange = (event) => {
+        this.setState({
+            username: event.target.value
+        });
+    };
+
+    handlePasswordChange = (event) => {
+        this.setState({
+            password: event.target.value
+        });
+    };
+
+    handleLogin = () => {
+        const { onLogin } = this.props;
+        const { username, password } = this.state;
+        onLogin({ username, password });
+    };
+
+    render() {
+        const { username, password } = this.state;
+        const isDisabled = !username || !password;
+
+        return (
+            <div>
+                <input
+                    type="text"
+                    value={username}
+                    onChange={this.handleUsernameChange}
+                    placeholder="Username"
+                />
+                <input
+                    type="password"
+                    value={password}
+                    onChange={this.handlePasswordChange}
+                    placeholder="Password"
+                />
+                <button onClick={this.handleLogin} disabled={isDisabled}>
+                    Login
+                </button>
+            </div>
+        );
+    }
+}
+
+export default Login;
