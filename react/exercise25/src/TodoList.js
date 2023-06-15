9cd6b0532cd7057320e531fa7a637ac851bee5da
Alptekin <alptekin.ocakdan@gmail.com>
2023-06-15 20:07:47 +0300

    Lists - Add a "reset" `button`

---
+++ react/exercise25/src/TodoList.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+
+class TodoList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [],
+            newItem: ''
+        };
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            newItem: event.target.value
+        });
+    };
+
+    handleAddItem = () => {
+        const { newItem, items } = this.state;
+        if (newItem.trim() !== '') {
+            this.setState({
+                items: [...items, newItem],
+                newItem: ''
+            });
+        }
+    };
+
+    handleReset = () => {
+        this.setState({
+            items: []
+        });
+    };
+
+    render() {
+        const { items, newItem } = this.state;
+
+        return (
+            <div>
+                <ul>
+                    {items.map((item, index) => (
+                        <li key={index}>{item}</li>
+                    ))}
+                </ul>
+                <input
+                    type="text"
+                    value={newItem}
+                    onChange={this.handleChange}
+                    placeholder="Add an item"
+                />
+                <button onClick={this.handleAddItem}>Add</button>
+                <button onClick={this.handleReset}>Reset</button>
+            </div>
+        );
+    }
+}
+
+export default TodoList;
