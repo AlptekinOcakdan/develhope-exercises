a1c91a20c5b9d238bbd15c3c932db7c2aee56256
Alptekin <alptekin.ocakdan@gmail.com>
2023-06-18 01:12:23 +0300

    React Router - Add a Not Found route

---
+++ react/exercise47/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import ShowGithubUser from './ShowGithubUser';
+import Counter from './Counter';
+import Home from './Home';
+import NotFound from './NotFound';
+// I intentionally didn't add the components mentioned on code.
+
+const App = () => {
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/counter">Counter</Link>
+                        </li>
+                        <li>
+                            <Link to="/users/AlptekinOcakdan">User: AlptekinOcakdan</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/counter" component={Counter} />
+                    <Route path="/users/:username" component={ShowGithubUser} />
+                    <Route component={NotFound} />
+                </Switch>
+            </div>
+        </Router>
+    );
+};
+
+export default App;
