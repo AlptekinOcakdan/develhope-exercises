c41d6fbd5d8e1227ce1091a8aae4bf8bca237750
Alptekin <alptekin.ocakdan@gmail.com>
2023-06-18 01:01:45 +0300

    React Router - Add the `/counter` path

---
+++ react/exercise44/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Counter from './Counter';
+
+const App = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route path="/counter" component={Counter} />
+            </Switch>
+        </Router>
+    );
+};
+
+export default App;
