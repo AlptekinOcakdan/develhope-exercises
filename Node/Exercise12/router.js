fd141adedbd5514d7bc6c1d37da46b377117867e
AlptekinOcakdan <alptekin.ocakdan@gmail.com>
2023-08-21 14:08:05 +0300

    Add Controllers

--- Node/Exercise12/router.js
+++ Node/Exercise12/router.js
@@ -3,19 +3,14 @@ import * as planetsController from './controllers/planet';
 
 const router = express.Router();
 
-// GET all planets
 router.get('/api/planets', planetsController.getAll);
 
-// GET a planet by ID
 router.get('/api/planets/:id', planetsController.getOneById);
 
-// POST a new planet
 router.post('/api/planets', planetsController.create);
 
-// PUT (update) a planet by ID
 router.put('/api/planets/:id', planetsController.updateById);
 
-// DELETE a planet by ID
 router.delete('/api/planets/:id', planetsController.deleteById);
 
 export default router;
